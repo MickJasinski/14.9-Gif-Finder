26b0417e55ace6de7687d33987f365bc6a89e30e
Mick Jasinski <m.jasinski@minimalcraft.net>
2018-01-27 23:40:08 +0000

    Fix handleKeyUp

--- components/Search.js
+++ components/Search.js
@@ -29,6 +29,7 @@ Search = React.createClass({
     return <input
       type = "text"
       onChange = { this.handleChange }
+      onKeyUp = { this.handleKeyUp }
       placeholder = "Tutaj wpisz wyszukiwan frazę"
       style = { styles }
       value = { this.state.searchTerm }
